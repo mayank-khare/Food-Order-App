0f5b510ab4214b64aaa270346e5263d0f702bece
Mayank Khare <mayankkhare92@gmail.com>
2021-12-07 15:13:58 +0000

    Added Cart, MealItem and Input component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import classes from './Cart.module.css';
+
+const Cart = props => {
+  const cartItems = [{id: 'c1', name: 'Sushi', amount: 2, price: 12.99},].map(())
+
+  return <div>
+  cartItems
+  <div></div>
+  <div></div>
+  </div>
+};
+
+export default Cart;
